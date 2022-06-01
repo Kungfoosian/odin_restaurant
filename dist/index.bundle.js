f0405698b3e60b30afdb6378d01c6dedb26fa740
Anthony <anthonyvovo123@gmail.com>
2022-06-01 12:08:58 -0700

    setting up

---
+++ dist/index.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("console.log('hello');\n\n// function component() {\n//   const element = document.createElement('div');\n//  const btn = document.createElement('button');\n\n//   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');\n\n//  btn.innerHTML = 'Click me and check the console!';\n// //  btn.onclick = printMe;\n\n//  element.appendChild(btn);\n\n//   return element;\n// }\n\n// document.body.appendChild(component());\n\n//# sourceURL=webpack://odin_restaurant/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
